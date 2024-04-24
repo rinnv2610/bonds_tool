64ff8f269e22a1533bbbdcbd00a01fd11289dee3
rinnv <vietrinit@gmail.com>
2024-04-24 18:07:14 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import time
+import os
+from dotenv import load_dotenv
+
+from bs4 import BeautifulSoup
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+from telegram import TelegramBot
+
+load_dotenv()
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+
+def crawl_data(chain):
+    with Chrome(options=chrome_options) as browser:
+        url = ''.join([os.getenv("APE_BONDS_URL"), str(chain)])
+        browser.get(url)
+        time.sleep(5)
+        html = browser.page_source
+
+    try:
+        soup = BeautifulSoup(html, "html.parser")
+        tokens = soup.find_all("div", {"class": "css-19aks3c"})
+        chain_name = soup.find("span", {"class": "css-w7gdm9"}).text
+        result = f"-------------{chain_name} CHAIN------------"
+
+        for idx, token in enumerate(tokens):
+            # name_token = token.find("div", {"class": "css-dlmr47"}).text
+            # img = token.find("img").get('src')
+            # type_token = token.find("div", {"class": "css-1e8v7uh"}).text
+            symbol = token.find("span", {"class": "css-11u4bai"}).text
+            discount_percent = token.find("span", {"class": "css-1hsh4tf"})
+
+            if discount_percent:
+                result = '\n'.join([result, f'{symbol}: {discount_percent.text}'])
+
+        return result
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    chains = eval(os.getenv("CHAINS"))
+    message = ''
+
+    for chain in chains:
+        message = '\n\n'.join([message, crawl_data(chain)])
+
+    telegram_bot = TelegramBot()
+    telegram_bot.send_message_to_channel(message)
---
+++ telegram.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class TelegramBot:
+    def __init__(self):
+        self.bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+        self.send_message_to_channel = os.getenv('TELEGRAM_BOT_SEND_MESSAGE_CHANNEL_URL')
+        self.chat_id = os.getenv('CHAT_ID')
+
+    def send_message_to_channel(self, message):
+        url = self.send_message_to_channel.format(bot_token=self.bot_token)
+
+        try:
+            response = requests.post(url, json=dict(chat_id=self.chat_id, text=message))
+
+            if response.status_code == 200:
+                print('Message sent successfully!')
+            else:
+                print('Failed to send message:', response.text)
+
+        except Exception as e:
+            print(e)
