e1f3b1534e2c627bb74d2e24b22f083493c943eb
rinnv <vietrinit@gmail.com>
2024-04-25 14:37:37 +0700

    fix data type

--- cron_job.py
+++ cron_job.py
@@ -3,7 +3,7 @@ from main import do_process
 import time
 from config import Config
 
-schedule.every(Config.BOT_RUN_TIME).minutes.do(do_process)
+schedule.every(int(Config.BOT_RUN_TIME)).minutes.do(do_process)
 
 while True:
     print("cron job is running")
